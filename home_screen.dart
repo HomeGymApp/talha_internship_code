87c37d22707f6f7e84b4823d66a60ff03ec16b18
hafiztalhaiqbal <166757697+hafiztalhaiqbal@users.noreply.github.com>
2025-08-12 08:13:14 +0500

    Add files via upload

--- home_screen.dart
+++ home_screen.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:multirole/main.dart';
-import 'package:multirole/login_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:rest_api/modals/PosterModel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,15 +11,48 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<PosterModel> postList = [];
+
+  Future<List<PosterModel>> getPostApi() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body.toString());
+      postList = data.map((item) => PosterModel.fromJson(item)).toList();
+      return postList;
+    } else {
+      return [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text("jenab")
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text('API Integration Example'),
       ),
-
+      body: FutureBuilder<List<PosterModel>>(
+        future: getPostApi(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return ListView.builder(
+              itemCount: postList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(postList[index].title),
+                  subtitle: Text(postList[index].body),
+                  leading: Text(postList[index].id.toString()),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:multirole/home_screen.dart';
-import 'package:multirole/login_screen.dart';
+import 'package:rest_api/example_2.dart';
+import 'package:rest_api/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,15 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-
-      home: const LoginScreen(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ExampleTwo(),
     );
   }
 }
-
-
